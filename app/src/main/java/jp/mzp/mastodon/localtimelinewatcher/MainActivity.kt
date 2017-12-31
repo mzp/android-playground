a9e74117df36a34e776ae910ed26fa9edcea57e9
mzp <mzpppp@gmail.com>
2017-12-31 10:03:41 +0900

    :art: add drawer menu

--- app/src/main/java/jp/mzp/mastodon/localtimelinewatcher/MainActivity.kt
+++ app/src/main/java/jp/mzp/mastodon/localtimelinewatcher/MainActivity.kt
@@ -2,11 +2,23 @@ package jp.mzp.mastodon.localtimelinewatcher
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.Menu
+import android.R.menu
+import android.os.PersistableBundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    val drawerToggle: ActionBarDrawerToggle by lazy {
+        ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.drawer_open, R.string.drawer_close)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
     }
 }
