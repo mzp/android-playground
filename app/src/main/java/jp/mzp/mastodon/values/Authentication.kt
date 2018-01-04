a0c8c181001f053eb2449384985aac1b3042a9df
mzp <mzpppp@gmail.com>
2018-01-04 11:36:44 +0900

    :bug: fix value class to store in parcel

--- app/src/main/java/jp/mzp/mastodon/values/Authentication.kt
+++ app/src/main/java/jp/mzp/mastodon/values/Authentication.kt
@@ -2,6 +2,7 @@ package jp.mzp.mastodon.values
 
 import android.os.Parcelable
 import auto.parcelgson.AutoParcelGson
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import com.sys1yagi.mastodon4j.api.entity.auth.AccessToken
 
@@ -10,12 +11,19 @@ abstract class Authentication() : Parcelable {
 
     @SerializedName("hostName")
     abstract fun hostName(): String
-    @SerializedName("accessToken")
-    abstract fun accessToken(): AccessToken
+    @SerializedName("accessTokenRaw")
+    abstract fun accessTokenRaw(): String
+
+    fun accessToken(): AccessToken {
+        val gson = Gson()
+        return gson.fromJson<AccessToken>(this.accessTokenRaw(), AccessToken::class.java)
+    }
 
     companion object {
         fun create(hostName: String, accessToken: AccessToken): Authentication {
-            return AutoParcelGson_Authentication(hostName, accessToken)
+            val gson = Gson()
+            val accessTokenRaw = gson.toJson(accessToken)
+            return AutoParcelGson_Authentication(hostName, accessTokenRaw)
         }
     }
 }
