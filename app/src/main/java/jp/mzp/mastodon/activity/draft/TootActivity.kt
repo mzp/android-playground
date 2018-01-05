d8fcbdad6ebf845211592c6bd82084d960dd791c
mzp <mzpppp@gmail.com>
2018-01-05 21:57:51 +0900

    :art: add toot dialog

---
+++ app/src/main/java/jp/mzp/mastodon/activity/draft/TootActivity.kt
@@ -0,0 +1,12 @@
+package jp.mzp.mastodon.activity.draft
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toot)
+    }
+}
